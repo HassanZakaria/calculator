3702ad16a3d1b3beb26b492819131f83208f6198
Hassan Zakaria <hassanzakaria14@gmail.com>
2021-01-14 20:03:20 +0100

    defined getClassNameFromNumber function

--- src/Components/Numbers.js
+++ src/Components/Numbers.js
@@ -2,8 +2,35 @@ import React from "react";
 import { Button } from "react-bootstrap";
 
 const Numbers = ({ number }) => {
+  function getClassNameFromNumber(num) {
+    switch (num) {
+      case 1:
+        return "one";
+      case 2:
+        return "two";
+      case 3:
+        return "three";
+      case 4:
+        return "four";
+      case 5:
+        return "five";
+      case 6:
+        return "six";
+      case 7:
+        return "seven";
+      case 8:
+        return "eight";
+      case 9:
+        return "nine";
+      case 0:
+        return "zero";
+      default:
+        console.error("error occurred! Unknown number");
+    }
+  }
+
   return (
-    <Button variant="primary" className="calc-btn">
+    <Button variant="primary" className={getClassNameFromNumber(number)}>
       {number}
     </Button>
   );
